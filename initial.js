15c9bdf5bed8df9d6dd9863b3772d4a0fc822d02
Zachary Orona-Calvert <oronazac@gmail.com>
2017-12-21 19:02:50 -0800

    Update initial.js

--- initial.js
+++ initial.js
@@ -4,7 +4,7 @@ const Snoostorm = require("snoostorm");
 
 // Build Snoowrap and Snoostorm clients
 const r = new Snoowrap({
-  userAgent: "reddit-bot-example-node",
+  userAgent: "BTCpricebot",
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
   username: process.env.REDDIT_USER,
@@ -13,8 +13,9 @@ const r = new Snoowrap({
 const client = new Snoostorm(r);
 // Configure options for stream: subreddit & results per query
 const streamOpts = {
-  subreddit: "btc",
-  results: 100
+  subreddit: "all",
+  results: 100,
+  pollTime: 2000,
 };
 // Create a Snoostorm CommentStream with the specified options
 const comments = client.CommentStream(streamOpts);
@@ -22,41 +23,44 @@ const comments = client.CommentStream(streamOpts);
 // On comment, perform whatever logic you want to do
 comments.on("comment", comment => {
   console.log(comment);
-  var https = require("https");
+const coindesk = require('node-coindesk-api')
+ 
+coindesk.getCurrentPrice().then(function (data) {
+  
 
-  https.get(
-    {
-      host: "api.coindesk.com",
-      path: "/v1/bpi/currentprice.json"
-    },
-    function(response) {
-      // Continuously update stream with data
-      var body = "";
-      response.on("data", function(d) {
-        body += d;
-      });
-      response.on("end", function() {
-        // Data reception is done, do whatever with it!
-        var parsed = JSON.parse(body);
-        console.log(parsed.bpi.USD.rate);
-        var USD = parsed.bpi.USD.rate_float;
-        var GPB = parsed.bpi.GBP.rate_float;
-        var EU = parsed.bpi.EUR.rate_float;
-        if (comment.body === "!price") {
-          comment.reply(
-            "The current exchange rate of BTC is: " +
-            "1 BTC = " +
-            USD +
-            " USD." +
-            " 1 BTC = " +
-            GPB +
-            " GPB." +
-            " 1 BTC = " +
-            EU +
-            " EU."
-          );
-        }
-      });
-    }
-  );
+        
+        var USD = data.bpi.USD.rate_float;
+        var GPB = data.bpi.GBP.rate_float;
+        var EU = data.bpi.EUR.rate_float;
+       if (comment.body.includes("bitcoin's price")){                                                                                                                                         
+                                                                                                                                                                                                                                       
+
+ comment.reply("The current exchange rate of BTC is: " +
+"1 BTC = " + USD + " USD." + 
+" 1 BTC = " + GPB + " GPB." +
+" 1 BTC = " + EU + " EU.");}
+
+
+if (comment.body.includes("!price")){                                                                                                                                         
+                                                                                                                                                                                                                                       
+
+ comment.reply("The current exchange rate of BTC is: " +
+"1 BTC = " + USD + " USD." + 
+" 1 BTC = " + GPB + " GPB." +
+" 1 BTC = " + EU + " EU.");}
+
+if (comment.body.includes("bitcoins price")){                                                                                                                                         
+                                                                                                                                                                                                                                       
+
+ comment.reply("The current exchange rate of BTC is: " +
+"1 BTC = " + USD + " USD." + 
+" 1 BTC = " + GPB + " GPB." +
+" 1 BTC = " + EU + " EU.");}
+    
+})
 });
+  
+
+
+  
+
